6da04a568249774c864c7a01a0925c11d80dfd4a
af-ahfz <wargablokbbukitsultan@gmail.com>
2026-06-29 11:37:37 +0700

    Refactor service worker for improved caching

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,59 @@
-const CACHE_NAME = "ebukitsultan-v1";
-
-const urlsToCache = [
-  "/eBukitSultan/",
-  "/eBukitSultan/index.html",
-  "/eBukitSultan/manifest.webmanifest",
-  "/eBukitSultan/offline.html"
+// Service Worker untuk eBukitSultan PWA
+const CACHE_NAME = 'ebukitsultan-v1.0.0';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/icons/icon-72.png',
+  '/icons/icon-96.png',
+  '/icons/icon-128.png',
+  '/icons/icon-144.png',
+  '/icons/icon-152.png',
+  '/icons/icon-192.png',
+  '/icons/icon-384.png',
+  '/icons/icon-512.png'
 ];
 
-self.addEventListener("install", event => {
+// Install Service Worker
+self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(function(cache) {
+        console.log('Cache opened');
+        return cache.addAll(ASSETS);
+      })
+      .then(function() {
+        return self.skipWaiting();
+      })
   );
-  self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+// Activate Service Worker
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
 });
 
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-
+// Fetch from cache
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    fetch(event.request)
-      .catch(() => caches.match(event.request))
-      .then(response => {
-        return response || caches.match("/eBukitSultan/offline.html");
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
       })
   );
 });
